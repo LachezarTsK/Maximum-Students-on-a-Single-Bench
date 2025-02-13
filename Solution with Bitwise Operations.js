a3f3924660d35fc7e467393bac317f54c0b073b3
Lachezar Kutsarov <lkutsarov@gmail.com>
2025-02-13 19:15:13 +0100

    Create Solution with Bitwise Operations.js

---
+++ Solution with Bitwise Operations.js
@@ -0,0 +1,37 @@
+
+/**
+ * @param {number[][]} students
+ * @return {number}
+ */
+var maxStudentsOnBench = function (students) {
+    const ID_RANGE = [1, 100];
+    let maxStudentsOnBench = 0;
+    const frequencyStudentsPerBench = new Array(ID_RANGE[1] + 1).fill(0);
+    const bitStampForSeatedStudentsOnBench = Array.from(new Array(ID_RANGE[1] + 1), () => new Array(4).fill(0));
+
+    for (let current of students) {
+        const studentID = current[0];
+        const benchID = current[1];
+
+        const index = Math.floor(studentID / (1 + Math.floor(ID_RANGE[1] / 4)));
+        const bitShifts = studentID % (1 + Math.floor(ID_RANGE[1] / 4));
+        const bitStampStudent = 1 << bitShifts;
+
+        if (!isSeated(bitStampForSeatedStudentsOnBench[benchID][index], bitStampStudent)) {
+            ++frequencyStudentsPerBench[benchID];
+            bitStampForSeatedStudentsOnBench[benchID][index] ^= bitStampStudent;
+            maxStudentsOnBench = Math.max(maxStudentsOnBench, frequencyStudentsPerBench[benchID]);
+        }
+    }
+
+    return maxStudentsOnBench;
+};
+
+/**
+ * @param {number} bitStampForSeatedStudentsOnBench
+ * @param {number} bitStampStudent
+ * @return {boolean}
+ */
+function isSeated(bitStampForSeatedStudentsOnBench, bitStampStudent) {
+    return (bitStampForSeatedStudentsOnBench & bitStampStudent) !== 0;
+}
