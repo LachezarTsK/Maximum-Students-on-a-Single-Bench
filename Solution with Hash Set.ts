d3687d8174d9664cdd91caec9bfd9c9eb0a50d70
Lachezar Kutsarov <lkutsarov@gmail.com>
2025-02-13 19:15:54 +0100

    Create Solution with Hash Set.ts

---
+++ Solution with Hash Set.ts
@@ -0,0 +1,21 @@
+
+function maxStudentsOnBench(students: number[][]): number {
+    const ID_RANGE = [1, 100];
+    const frequencyStudentsPerBench = new Array(ID_RANGE[1] + 1).fill(0);
+    const seatedStudentsOnBench = new Array(ID_RANGE[1] + 1);
+    for (let i = 0; i < seatedStudentsOnBench.length; ++i) {
+        seatedStudentsOnBench[i] = new Set();
+    }
+
+    let maxStudentsOnBench = 0;
+    for (let [studentID, benchID] of students) {
+
+        if (!seatedStudentsOnBench[benchID].has(studentID)) {
+            ++frequencyStudentsPerBench[benchID];
+            seatedStudentsOnBench[benchID].add(studentID);
+            maxStudentsOnBench = Math.max(maxStudentsOnBench, frequencyStudentsPerBench[benchID]);
+        }
+    }
+
+    return maxStudentsOnBench;
+};
